9cb02b43a041e503a70e82f7b384a7ea0de3bc41
Giacomo Cimini <management@liriusgames.com>
2025-11-21 16:49:30 +0100

    Update AOCPerformHypeState.class.php

--- modules/states/AOCPerformHypeState.class.php
+++ modules/states/AOCPerformHypeState.class.php
@@ -1,21 +1,7 @@
 <?php
-
 /**
- *------
- * BGA framework: © Gregory Isabelli <gisabelli@boardgamearena.com> & Emmanuel Colin <ecolin@boardgamearena.com>
- * AgeOfComics implementation : © Evan Pulgino <evan.pulgino@gmail.com>
- *
- * This code has been produced on the BGA studio platform for use on http://boardgamearena.com.
- * See http://en.boardgamearena.com/#!doc/Studio for more information.
- * -----
- *
  * Backend functions used by the performHype State
- *
- * During this state, the player may choose a comic in their hand to hype
- *
- * @EvanPulgino
  */
-
 class AOCPerformHypeState {
     private $game;
 
@@ -23,14 +9,54 @@ class AOCPerformHypeState {
         $this->game = $game;
     }
 
-    /**
-     * Gets the list of args used by the PerformHype state
-     *
-     * Args:
-     *
-     * @return array The list of args used by the PerformHype state
-     */
     public function getArgs($playerId = null) {
         return [];
+    }
+
+    public function hypeComic($cardId) {
+        $playerId = $this->game->getPlayerId();
+        
+        // 1. Security Check: Does the card belong to the player?
+        $card = $this->game->cardManager->getCard($cardId);
+        if ($card['location'] != 'hand' || $card['player_id'] != $playerId) {
+            throw new BgaUserException("You can only hype cards from your hand.");
+        }
+
+        // 2. Security Check: Is it a Comic?
+        // Assuming type_id 3 is Comic and 4 is Ripoff (based on standard BGA logic)
+        // You might need to check CONSTANTS if this fails, but usually type is string 'comic'
+        if ($card['type'] != 'comic' && $card['type'] != 'ripoff') {
+             throw new BgaUserException("You can only hype Comic Books.");
+        }
+
+        // 3. Move the Card
+        // We move it to a special location so it doesn't count towards Hand Limit
+        // location_arg 2 represents "2 Fans" (The Hype Token value)
+        $this->game->cardManager->moveCard($cardId, 'hyped', $playerId);
+        
+        // 4. Add the "Hype Token" (Visuals handled by Notification, Data by location_arg)
+        // We use a database update to ensure the token value (2) is saved
+        // If your cardManager has a specific function for tokens, use that, otherwise:
+        $sql = "UPDATE card SET card_location_arg = 2 WHERE card_id = $cardId";
+        $this->game->DbQuery($sql);
+
+        // 5. Notify Players
+        $this->game->notifyAllPlayers(
+            "hypeComic",
+            clienttranslate('${player_name} hypes a comic! It gains a Hype Token (2 Fans).'),
+            [
+                'player_name' => $this->game->getActivePlayerName(),
+                'card' => $card,
+                'cardId' => $cardId
+            ]
+        );
+
+        // 6. End State
+        $this->game->gamestate->nextState("nextPlayerTurn");
+    }
+
+    public function skipHype() {
+        // Just move to the next state
+        $this->game->gamestate->nextState("nextPlayerTurn");
     }
 }
