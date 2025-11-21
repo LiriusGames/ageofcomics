e08cf57963853b4ef6aea003c5c1a61dc9307b68
Giacomo Cimini <management@liriusgames.com>
2025-11-21 17:50:26 +0100

    Update AOCPerformBetterColorsState.class.php

--- modules/states/AOCPerformBetterColorsState.class.php
+++ modules/states/AOCPerformBetterColorsState.class.php
@@ -1,21 +1,7 @@
 <?php
-
 /**
- *------
- * BGA framework: © Gregory Isabelli <gisabelli@boardgamearena.com> & Emmanuel Colin <ecolin@boardgamearena.com>
- * AgeOfComics implementation : © Evan Pulgino <evan.pulgino@gmail.com>
- *
- * This code has been produced on the BGA studio platform for use on http://boardgamearena.com.
- * See http://en.boardgamearena.com/#!doc/Studio for more information.
- * -----
- *
  * Backend functions used by the performBetterColors State
- *
- * During this state, the player gains a better color token on their newly printed comic
- * 
- * @EvanPulgino
  */
-
 class AOCPerformBetterColorsState {
     private $game;
 
@@ -23,18 +9,50 @@ class AOCPerformBetterColorsState {
         $this->game = $game;
     }
 
-    /**
-     * Gets the list of args used by the PerformBetterColors state
-     *
-     * Args:
-     *
-     * @return array The list of args used by the PerformBetterColors state
-     */
     public function getArgs($playerId = null) {
         return [];
     }
 
-    public function stGainBetterColorToken() {
-        // Gain the better color token
+    /**
+     * Logic to apply the Better Color Token
+     * This is called when the player clicks the "Collect" button
+     */
+    public function gainBetterColor() {
+        $player = $this->game->playerManager->getActivePlayer();
+        $playerId = $player->getId();
+
+        // 1. Find the most recently printed comic for this player
+        // We sort by ID descending to get the newest one (the one just printed)
+        $sql = "SELECT * FROM mini_comic WHERE mini_comic_owner = $playerId ORDER BY mini_comic_id DESC LIMIT 1";
+        $comic = $this->game->getObjectFromDB($sql);
+
+        if (!$comic) {
+            // Should not happen if logic is correct, but safety first:
+            $this->game->gamestate->nextState("checkUpgrade");
+            return;
+        }
+
+        $comicId = $comic['mini_comic_id'];
+
+        // 2. Mark the Comic in the Database
+        // We need a way to flag "Has Better Color". 
+        // We will add +100 to the 'type_arg' column. 
+        // (Assuming type_arg is usually < 100. This acts as a "flag" we can check during scoring).
+        $this->game->DbQuery("UPDATE mini_comic SET mini_comic_type_arg = mini_comic_type_arg + 100 WHERE mini_comic_id = $comicId");
+
+        // 3. Notify the Frontend
+        // This triggers the JS we wrote earlier to display the 'better_color.png' token
+        $this->game->notifyAllPlayers(
+            "gainBetterColorToken",
+            clienttranslate('${player_name} adds Better Colors to the print run! (+2 VP at end of game)'),
+            [
+                'player_name' => $player->getName(),
+                'player' => $player->getUiData(),
+                'miniComicId' => $comicId
+            ]
+        );
+
+        // 4. Move to the next step (The Upgrade Cube)
+        $this->game->gamestate->nextState("checkUpgrade");
     }
 }
